a9a030c2a82f0f88535d54d4f7d5e85a7e92483d
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-01 07:56:27 -0500

    16*8 tile grid

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,62 @@
 package;
 
+import flixel.math.FlxPoint;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var tiles:FlxTypedSpriteGroup<Tile>;
+
 	override public function create()
 	{
+		tiles = new FlxTypedSpriteGroup<Tile>();
+		add(tiles);
+
+		/**
+		makeGrid(FlxPoint.weak(20, 12), null, function(tile:Tile)
+		{
+			tile.alpha = 0.25;
+			tile.scale.set(0.5, 0.5);
+
+			final ix = Math.floor(tile.x / 64);
+			final iy = Math.floor(tile.y / 64);
+
+			if (ix > 1 && ix < 17)
+				if (iy > 1 && iy < 10)
+					tile.destroy();
+		});
+		 */
+
+		makeGrid(FlxPoint.weak(16, 8), FlxPoint.weak(2, 2));
+
 		super.create();
+	}
+
+	function makeGrid(dimensions:FlxPoint, ?offset:FlxPoint, ?onTileMade:Tile->Void)
+	{
+		var ix = 0;
+		var iy = 0;
+
+		while (iy < dimensions.y)
+		{
+			while (ix < dimensions.x)
+			{
+				var tile = new Tile();
+				tile.setPosition((ix + ((offset != null) ? offset.x : 0)) * 64, (iy + ((offset != null) ? offset.y : 0)) * 64);
+
+				if (onTileMade != null)
+					onTileMade(tile);
+
+				if (tile != null && tile.exists)
+					tiles.add(tile);
+
+				ix++;
+			}
+
+			ix = 0;
+			iy++;
+		}
 	}
 
 	override public function update(elapsed:Float)
---
+++ source/Tile.hx
@@ -0,0 +1,12 @@
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+
+class Tile extends FlxSprite
+{
+	override public function new()
+	{
+		super();
+
+		makeGraphic(64, 64);
+	}
+}
